686142ed2bc74976ded89c96ce8d9925f7a531f2
dangducloc <121280030+dangducloc@users.noreply.github.com>
2023-12-18 05:50:30 +0700

    change endpoint of navbar

--- public_html.Admin/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-function generateHexArray(length) {
-    let hexArray = [];
-    for (let i = 0; i < length; i++) {
-      let hexValue = Math.floor(Math.random() * 256).toString(16).padStart(2, '0');
-      hexArray.push(hexValue);
-    }
-    return hexArray;
-  }
